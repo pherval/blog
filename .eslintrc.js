3c0d51f74aa1f45a403f6f30cc0b00fb6f7c0f81
Peter <github@peter.dev.br>
2022-02-01 10:50:49 -0300

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
